016ea1537d269b5dc3124ef38df2732881e9f6f5
fredpell1 <fredericpelletier7@gmail.com>
2024-07-14 17:51:14 -0400

    basic build system (to review later)

---
+++ app/basic-app/main.C
@@ -0,0 +1,7 @@
+#include "basic-app/basic_dep.H"
+
+int main(int argc, char** argv)
+{
+    sayHello();
+    return 0;
+}
---
+++ lib/basic-app/basic_dep.C
@@ -0,0 +1,6 @@
+#include "basic-app/basic_dep.H"
+
+void sayHello(void)
+{
+    std::cout << "Hello, world!" << std::endl;
+}
